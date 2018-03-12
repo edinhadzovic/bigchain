945b149484606ae5ee1c8984926d2f4942877267
Edin Hadzovic <edin@midlight.gmbh>
2018-03-12 15:47:57 +0100

    adding loginView.html

--- App/main.js
+++ App/main.js
@@ -28,11 +28,12 @@ async function createWindow () {
   ];
 
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow();
+  mainWindow.maximize();
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, '../ClientInterface/views/loginView.html'),
     protocol: 'file:',
     slashes: true
   }))
