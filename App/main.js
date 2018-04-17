0c628057712c352542fa20854a9f3c7cf496b5b1
Edin Hadzovic <edin@midlight.gmbh>
2018-04-17 17:39:05 +0200

    some small changes

--- App/main.js
+++ App/main.js
@@ -51,6 +51,7 @@ function createMainWindow (user, event) {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
+    app.emit('window-all-closed');
   });
 }
 
