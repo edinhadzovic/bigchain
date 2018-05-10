ccf7ee01d5af30fe4b2ec5a72567c99cedc7efd9
Danilo Kuruzovic <kuruzovic@student.tugraz.at>
2018-05-10 18:48:08 +0200

    eth

--- App/wallets/eth.js
+++ App/wallets/eth.js
@@ -10,6 +10,8 @@ var request = require('request');
 const eth_util = require('ethereumjs-util')
 const keythereum = require("keythereum");
 const EthereumTx = require('ethereumjs-tx')
+const ethers = require('ethers');
+
 
 // for hash
 const createKeccakHash = require("keccak/js");
@@ -78,23 +80,54 @@ class Ethereum {
             data.s Buffer EC signature parameter
             data.value Buffer the amount of ether sent
 
-        */
+        
+       let gasPrice = 100000;
+       let gasLimit = 500000;
         const txParams = {
-            nonce: 0,
-            gasPrice: 100, 
-            gasLimit: 1000,
-            to: address, 
+            nonce: '0x00',
+            gasPrice: '0x4800', 
+            gasLimit: '0x0',
+            to: address,
             data: messageBuffer,
             value: amountSatoshi, 
             // EIP 155 chainId - mainnet: 1, ropsten: 3 ROPSTEN IS USED AS TESTNET 
             chainId: 3
         }
 
+
         const tx = new EthereumTx(txParams);
         console.log(eth_util.toBuffer(wallet._eth_privateKey));
+
+        var feeCost = tx.getUpfrontCost()
+        tx.gas = feeCost.toString('hex');
+
         tx.sign(eth_util.toBuffer(wallet._eth_privateKey));
         const serializedTx = tx.serialize();
-        console.log(serializedTx.toString('hex'));
+        console.log(serializedTx.toString('hex')); 
+        */
+
+       var Wallet = ethers.Wallet;
+       var utils = ethers.utils;
+       var wall = new ethers.Wallet(eth_util.toBuffer(wallet._eth_privateKey));
+
+        var transaction = {
+            nonce: 0,
+            gasLimit: 21000,
+            gasPrice: utils.bigNumberify("20000000000"),
+        
+            to: address,
+        
+            value: utils.parseEther(amount),
+            data: "0x",
+            // This ensures the transaction cannot be replayed on different networks
+            chainId: 3
+        };
+
+        var signedTransaction = wall.sign(transaction);
+        console.log(wall);
+        var transaction = Wallet.parseTransaction(signedTransaction);
+        console.log(signedTransaction);
+        console.log(transaction);
         
     }
 
